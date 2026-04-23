4d4735a70681e5b73dc89f15913a1c0db16f3ef9
trondtynnol <tty001@uit.no>
2026-04-23 08:30:15 +0200

    Parse lemmas and create outline of further parsing

--- scripts/html2gtxml.py
+++ scripts/html2gtxml.py
@@ -3,6 +3,7 @@ Convert sme-fin html to GT-style xml.
 """
 import argparse
 from pathlib import Path
+import re
 
 MISSING_DEP_HELP = """
 cannot run due to missing dependencies. hint, run:
@@ -11,12 +12,63 @@ python -m venv venv && . venv/bin/activate && pip install lxml
 """
 
 try:
-    from lxml.etree import Element, SubElement, tostring
+    from lxml.etree import Element, SubElement, tostring, fromstring
 except ImportError:
     exit(MISSING_DEP_HELP)
 
+def prettyprint(element, **kwargs):
+    """Pretty print function for testing"""
+    xml = tostring(element, encoding="utf-8", pretty_print=True, **kwargs)
+    print(xml.decode(), end='')
+
+
+def clean_text(text: str):
+    """Remove ´, ~, text within parentheses and trailing/leading whitespace"""
+    return re.sub("´|~|[(][^)]*[)]", "", text).strip()
+
+
+def insert_lemmas(lemmaList: list, lemmaText: str):
+    """Split lemma text and insert lemmas into lemma list"""
+    lemmas = re.split(', | ~ ', lemmaText)
+    lemmaList.extend([clean_text(lemma) for lemma in lemmas])
+
+
+def create_lemma_list(tree):
+    lemmaList = []
+
+    lemmas = tree.xpath("strong")
+    for lemma in lemmas:
+        insert_lemmas(lemmaList, lemma.text)
+    
+    # Remove empty lemmas
+    lemmaList = [lemma for lemma in lemmaList if lemma != ""]
+
+    return lemmaList
+
+def extract_translations_and_examples(tree):
+    pass
+
+
+def add_entry(lemma: str, translations_and_examples):
+    pass
+
+
+def add_entries(root: Element, line: str):
+    tree = fromstring(line)
+    lemmaList = create_lemma_list(tree)
+    translations_and_examples = extract_translations_and_examples(tree)
+    for lemma in lemmaList:
+        add_entry(lemma, translations_and_examples)
+
+
 def lines_to_xml_bytestring(lines):
     root = Element("r")
+
+    for line in lines:
+        if not line.startswith("<p>"):
+            continue
+        add_entries(root, line)
+
     doctype = (
         '<!DOCTYPE r PUBLIC "-//DivvunGiellatekno//DTD '
         'Dictionaries//Multilingual" "../dtd/spasme.dtd">'
